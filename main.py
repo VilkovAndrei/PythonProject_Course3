9bbff5270cedd9dde8f6a4732032114baa61e6e3
Andry Vilkov <vilkov-l-nn@mail.ru>
2023-12-14 20:24:42 +0300

    edit README.md

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ while True:
     if not os.path.isfile(operations_data_full_path):
         print("Файл с данными операций не найден!")
         break
+
+    operations_list = utilits.load_data_ops(operations_data_full_path)
+
+    sorted_list = sorted(operations_list, key=lambda x: x['date'], reverse=True)
+    sorted_list = [d for d in sorted_list if d["state"] == "EXECUTED"]
+    operations = sorted_list[:5]
+
+    for operation in operations:
+        print(utilits.show_operation(operation))
+
     break
-
-operations_list = utilits.load_data_ops(operations_data_full_path)
-
-sorted_list = sorted(operations_list, key=lambda x: x['date'], reverse=True)
-sorted_list = [d for d in sorted_list if d["state"] == "EXECUTED"]
-operations = sorted_list[:5]
-
-for operation in operations:
-    print(utilits.show_operation(operation))
--- utils/utilits.py
+++ utils/utilits.py
@@ -6,7 +6,7 @@ def load_data_ops(file_name):
     """
     Загружает список операций из файла
     :param file_name: имя файла
-    :return:
+    :return operations: список операций
     """
     with open(file_name, mode='r', encoding='utf-8') as file:
         operations = json.load(file)
@@ -18,39 +18,39 @@ def load_data_ops(file_name):
 
 def show_operation(operation_dict):
     """
-    Вывод на экран информации по банковской операции
-    :param operation_dict:
-    :return:
+    Вывод на экран информации по банковской операции в заданном формате
+    :param operation_dict: словарь данных банковской операции
+    :return f-строка: описание банковской операции согласно заданного формата
     """
-    if operation_dict == {}:
+    if operation_dict == {}:  # для теста, проверка на пустой словарь есть в функции load_data_ops()
         return ""
 
     op_date = date.fromisoformat(operation_dict['date'][:10]).strftime("%d.%m.%Y")
-    operation_description = operation_dict["description"]
+    op_description = operation_dict["description"]
     if "from" not in operation_dict.keys():
-        operation_from = ""
+        op_from = ""
     else:
         if operation_dict["from"][:5] == "Счет ":
-            operation_from = "Счет " + hide_account_number(operation_dict["from"])
+            op_from = "Счет " + hide_account_number(operation_dict["from"])
         else:
-            operation_from = operation_dict["from"][:-16] + hide_card_number(operation_dict["from"])
+            op_from = operation_dict["from"][:-16] + hide_card_number(operation_dict["from"])
 
     if operation_dict["to"][:5] == "Счет ":
-        operation_to = "Счет " + hide_account_number(operation_dict["to"])
+        op_to = "Счет " + hide_account_number(operation_dict["to"])
     else:
-        operation_to = operation_dict["to"][:-16] + hide_card_number(operation_dict["to"])
+        op_to = operation_dict["to"][:-16] + hide_card_number(operation_dict["to"])
 
     op_amount = operation_dict["operationAmount"]["amount"]
     op_currency = operation_dict["operationAmount"]["currency"]["name"]
 
-    return f"{op_date} {operation_description}\n{operation_from} -> {operation_to}\n{op_amount} {op_currency}\n"
+    return f"{op_date} {op_description}\n{op_from} -> {op_to}\n{op_amount} {op_currency}\n"
 
 
 def hide_card_number(operation_card_number):
     """
     Скрывает часть номера карты
-    :param operation_card_number:
-    :return:
+    :param operation_card_number: номер карты
+    :return h_card_number: скрытый номер карты
     """
     if operation_card_number != '':
         card_number = operation_card_number[-16:]
@@ -64,8 +64,8 @@ def hide_card_number(operation_card_number):
 def hide_account_number(operation_account_number):
     """
     Скрывает часть номера счета
-    :param operation_account_number:
-    :return:
+    :param operation_account_number: номер счета
+    :return h_account_number: скрытый номер счета
     """
     if operation_account_number != '':
         account_number = operation_account_number[-20:]
